715bef79cf4965844fc4bc2d19e165eb5ad2a4e4
Uwe Simon <uwe.simon@t-systems.com>
2020-03-29 23:51:24 +0200

    last day now shown, status line with day and total population

--- main.js
+++ main.js
@@ -2,81 +2,81 @@
 
 let gDebug = 0;
 
-class Disease{
-  constructor(waitdays, infectiousdays, symptomdays, infectionrate, nosymptomrate, heavysymptomrate){
-    this.waitdays          = waitdays;
-    this.infectiousdays    = infectiousdays;
-    this.symptomdays       = symptomdays;
-    this.infectionrate     = infectionrate;
-    this.nosymptomrate     = nosymptomrate;
-    this.heavysymptomrate  = heavysymptomrate;
-//    console.dir(this);
+class Disease {
+  constructor(waitdays, infectiousdays, symptomdays, infectionrate, nosymptomrate, heavysymptomrate) {
+    this.waitdays = waitdays;
+    this.infectiousdays = infectiousdays;
+    this.symptomdays = symptomdays;
+    this.infectionrate = infectionrate;
+    this.nosymptomrate = nosymptomrate;
+    this.heavysymptomrate = heavysymptomrate;
+    //    console.dir(this);
   }
 }
 
-class Person{
-  constructor(id, x, y, resistant){
-    this.id           = id;
-    this.x            = x;
-    this.y            = y;
-    this.resistant    = resistant;
-    this.infectedBy   = null;
-    this.infected     = null;
-    this.infectious   = null;
-    this.symptom      = null;
+class Person {
+  constructor(id, x, y, resistant) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.resistant = resistant;
+    this.infectedBy = null;
+    this.infected = null;
+    this.infectious = null;
+    this.symptom = null;
     this.heavysymptom = false;
-    this.cured        = null;
-    this.contacts     = 0;
+    this.cured = null;
+    this.contacts = 0;
   }
 
-  getId(){
-    return(this.id);
+  getId() {
+    return (this.id);
   }
 
-  setInfected(day, id, disease){
+  setInfected(day, id, disease) {
     this.infectedBy = id;
-    this.infected   = day;
-    this.infectious = this.infected     + disease.waitdays;
-    if(Math.random()>=disease.nosymptomrate){
+    this.infected = day;
+    this.infectious = this.infected + disease.waitdays;
+    if (Math.random() >= disease.nosymptomrate) {
       this.heavysymptom = (Math.random() < disease.heavysymptomrate);
-      this.symptom      = this.infectious + disease.infectiousdays;
-      this.cured        = this.symptom    + disease.symptomdays;
+      this.symptom = this.infectious + disease.infectiousdays;
+      this.cured = this.symptom + disease.symptomdays;
     } else {
-      this.symptom      = this.infectious + disease.symptomdays;
-      this.cured        = this.infectious + disease.symptomdays;
+      this.symptom = this.infectious + disease.symptomdays;
+      this.cured = this.infectious + disease.symptomdays;
     }
-//  console.dir(this);
+    //  console.dir(this);
   }
 
-  getResistant(){
-    return(this.resistant);
+  getResistant() {
+    return (this.resistant);
   }
 
-  canBeInfected(today){
-    return(!this.resistant && !this.infected);
+  canBeInfected(today) {
+    return (!this.resistant && !this.infected);
   }
 
-  isInfectious (today){
-    return(this.infectious<=today && today<this.symptom);
+  isInfectious(today) {
+    return (this.infectious <= today && today < this.symptom);
   }
 
-  isIll(today){
-    return(this.infected<=today && today<this.cured);
+  isIll(today) {
+    return (this.infected <= today && today < this.cured);
   }
 
-  hasSymptom(today){
-    return(this.symptom<=today && today<this.cured);
+  hasSymptom(today) {
+    return (this.symptom <= today && today < this.cured);
   }
 
-  hasHeavySymptom(today){
-    return(this.heavysymptom && this.symptom<=today && today<this.cured);
+  hasHeavySymptom(today) {
+    return (this.heavysymptom && this.symptom <= today && today < this.cured);
   }
 
-  incContacts(){
-    this.contacts ++;
+  incContacts() {
+    this.contacts++;
   }
 
-  dump(){
+  dump() {
     let txt = "";
     txt = txt + " " + this.infected;
     txt = txt + " " + this.infectious;
@@ -88,176 +88,184 @@ class Person{
     txt = txt + " " + this.contacts;
     txt = txt + " " + this.resistant;
     txt = txt + "\n";
-    return(txt);
+    return (txt);
   }
 }
 
 
-class Country{
-  constructor(xsize, ysize, period, resistantrate, distance, contacts){
-    this.xsize    = xsize;
-    this.ysize    = ysize;
-    this.period   = period;
+class Country {
+  constructor(xsize, ysize, period, resistantrate, distance, contacts) {
+    this.xsize = xsize;
+    this.ysize = ysize;
+    this.period = period;
     this.distance = distance;
     this.contacts = contacts;
-    this.country  = [];
+    this.country = [];
     this.infected = [];
-    this.status   = [];
-  
-    for(let i=0; i<=this.period; i++){
+    this.status = [];
+
+    for (let i = 0; i <= this.period; i++) {
       this.status[i] = {
-                        infected:     0,
-                        infectious:   0,
-                        heavysymptom: 0,
-                        symptom:      0,
-                        cured:        0
-                       };
+        infected: 0,
+        infectious: 0,
+        heavysymptom: 0,
+        symptom: 0,
+        cured: 0
+      };
     }
 
     let nr = 1;
-    for(let x=0; x<this.xsize; x++){
-      this.country[x]=[];
-      for(let y=0; y<this.ysize; y++){
-        this.setPerson(x, y, new Person(nr, x,y, Math.random()<resistantrate));
-        nr ++;
+    for (let x = 0; x < this.xsize; x++) {
+      this.country[x] = [];
+      for (let y = 0; y < this.ysize; y++) {
+        this.setPerson(x, y, new Person(nr, x, y, Math.random() < resistantrate));
+        nr++;
       }
     }
   }
 
-  setInfected(day, infected){
-    this.infected             = infected;
+  setInfected(day, infected) {
+    this.infected = infected;
     this.status[day].infected = infected.length;
   }
 
-  getInfected(){
-    return(this.infected);
+  getInfected() {
+    return (this.infected);
   }
 
-  personInfecting(day, person, disease){
+  personInfecting(day, person, disease) {
     let newInfected = [];
-    if(person.isIll(day)){
+    if (person.isIll(day)) {
       newInfected.push(person);
     }
-    if(person.isInfectious(day)){
-      this.status[day].infectious ++;
-      for(let i=0; i<this.contacts; i++){
-        if(Math.random()<disease.infectionrate){
-          let x = person.x + (Math.random()*2 -1) * this.distance |0;
-          let y = person.y + (Math.random()*2 -1) * this.distance |0;
-          if((x>=0 && x<this.xsize) && (y>=0 && y<this.ysize)){
-//        x = Math.max(Math.min(x, this.xsize-1),0);
-//        y = Math.max(Math.min(y, this.ysize-1),0);
-//        console.dir({x: x, y:y, px: person.x, py: person.y});
+    if (person.isInfectious(day)) {
+      this.status[day].infectious++;
+      for (let i = 0; i < this.contacts; i++) {
+        if (Math.random() < disease.infectionrate) {
+          let x = person.x + (Math.random() * 2 - 1) * this.distance | 0;
+          let y = person.y + (Math.random() * 2 - 1) * this.distance | 0;
+          if ((x >= 0 && x < this.xsize) && (y >= 0 && y < this.ysize)) {
+            //        x = Math.max(Math.min(x, this.xsize-1),0);
+            //        y = Math.max(Math.min(y, this.ysize-1),0);
+            //        console.dir({x: x, y:y, px: person.x, py: person.y});
             let newPerson = this.getPerson(x, y);
             newPerson.incContacts();
-            if(newPerson.canBeInfected(day)){
-              this.status[day].infected ++;
+            if (newPerson.canBeInfected(day)) {
+              this.status[day].infected++;
               newPerson.setInfected(day, person.getId(), disease);
               this.setPerson(x, y, newPerson);
               newInfected.push(newPerson);
             }
-//        } else{
-//          console.log('Pos', x, y);
+            //        } else{
+            //          console.log('Pos', x, y);
           }
         }
       }
     } else {
-      if(person.hasSymptom(day)){
-      if(person.hasHeavySymptom(day)){
-          this.status[day].heavysymptom ++;
+      if (person.hasSymptom(day)) {
+        if (person.hasHeavySymptom(day)) {
+          this.status[day].heavysymptom++;
         } else {
-          this.status[day].symptom ++;
+          this.status[day].symptom++;
         }
       } else {
-        this.status[day].cured ++;
+        this.status[day].cured++;
       }
     }
-    return(newInfected);
+    return (newInfected);
   }
 
-  calcInfections(day, disease){
+  calcInfections(day, disease) {
     let newInfected = [];
-    for(let person of this.infected){
-      let pInfected = this.personInfecting(day, person, disease);
-      for(let person of pInfected){
-        newInfected.push(person);
+    if (day < this.status.length) {
+      for (let person of this.infected) {
+        let pInfected = this.personInfecting(day, person, disease);
+        for (let person of pInfected) {
+          newInfected.push(person);
+        };
       };
     };
 
     this.infected = newInfected;
-    if(gDebug) { console.dir(infected); }
-    return(this.infected);
+    if (gDebug) {
+      console.dir(infected);
+    }
+    return (this.infected);
   }
 
-  setPerson(x,y, person){
+  setPerson(x, y, person) {
     this.country[x][y] = person;
   }
 
-  getPerson(x, y){
-    return(this.country[x][y]);
+  getPerson(x, y) {
+    return (this.country[x][y]);
   }
 
-  dump(){
-    let txt ="";
-    for(let x=0; x<xSize; x++){
-      for(let y=0; y<ySize; y++){
+  dump() {
+    let txt = "";
+    for (let x = 0; x < xSize; x++) {
+      for (let y = 0; y < ySize; y++) {
         txt = txt + this.country[x][y].dump();
       }
     }
-    return(txt);
+    return (txt);
   }
 
-  getStatus(day){
-    return(this.status[day]);
+  getStatus(day) {
+    return (this.status[day]);
   }
 }
 
 let disease = null;
 let country = null;
 
-class Simulation{
-  constructor(options){
+class Simulation {
+  constructor(options) {
     this.options = options;
-    this.today   = 1;
-    this.period  = options.period;
-    this.infected =[];
-    this.disease = new Disease(options.waitdays, options.infectiousdays, options.symptomdays, options.infectionrate/100.0, options.nosymptoms/100.0, options.heavysymptoms/100.0);
-    this.country = new Country(options.xsize, options.ysize, options.period, options.resistant/100.0, options.distance, options.contacts);
+    this.today = 0;
+    this.period = options.period;
+    this.infected = [];
+    this.disease = new Disease(options.waitdays, options.infectiousdays, options.symptomdays, options.infectionrate / 100.0, options.nosymptoms / 100.0, options.heavysymptoms / 100.0);
+    this.country = new Country(options.xsize, options.ysize, options.period, options.resistant / 100.0, options.distance, options.contacts);
 
   }
 
 
-  init(){
-    for(let i=0; i<this.options.initiallyinfected;){
-      let x = Math.random() * this.options.xsize |0;
-      let y = Math.random() * this.options.ysize |0;
+  init() {
+    for (let i = 0; i < this.options.initiallyinfected;) {
+      let x = Math.random() * this.options.xsize | 0;
+      let y = Math.random() * this.options.ysize | 0;
       let person = this.country.getPerson(x, y);
       person.incContacts();
-      if(person.canBeInfected()){
+      if (person.canBeInfected()) {
         person.setInfected(this.today, 0, this.disease);
         this.country.setPerson(x, y, person);
         this.infected.push(person);
         i++;
       }
     }
-  
+
     this.country.setInfected(this.today, this.infected);
   }
 
-  getData(){
-    if(this.today<=this.options.period){
-      return({country: this.country,
-              status: this.country.status,
-              day: this.today});
+  getData() {
+    if (this.today < this.country.status.length) {
+      return ({
+        country: this.country,
+        status: this.country.status,
+        day: this.today
+      });
     } else {
-      return(null);
-    }    
+      return (null);
+    }
   }
 
-  nextDay(){
-    this.today ++;
+  nextDay() {
+    this.today++;
     let infections = this.country.calcInfections(this.today, this.disease);
-    if(gDebug) {console.dir(infections);}
-    return(infections.length>0);
+    if (gDebug) {
+      console.dir(infections);
+    }
+    return (infections.length > 0);
   }
 }
